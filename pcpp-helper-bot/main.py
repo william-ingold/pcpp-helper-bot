e305d011a31fcfa4f127f5fc043941a2f95da7dd
William Ingold <github.ingold@gmail.com>
2021-01-08 10:35:59 -0500

    Segmented to be cleaner

--- pcpp-helper-bot/main.py
+++ pcpp-helper-bot/main.py
@@ -3,25 +3,38 @@ from markupcreator import MarkupCreator
 
 from pcpphelperbot import PCPPHelperBot
 
-if __name__ == '__main__':
-    test_url = 'https://pcpartpicker.com/list/PtCbTJ'
-    
-    pcpp = PCPartPickerList()
-    
-    # Load it locally
+
+def run_parse_local():
     test_html = 'C:/Users/willi/Development/PycharmProjects/pcpp-helper-bot/test-pages/page_two.htm'
     with open(test_html, 'r') as file:
-        page_html = file.read()
+        local_html = file.read()
         
+    dummy_url = 'www.TEST-URL.com'
+    return local_html, dummy_url
+
+
+def run_parse_live():
+    test_url = 'https://pcpartpicker.com/list/HQzZgJ'
+    live_html = pcpp.request_page_data(test_url)
+
+    return page_html, test_url
+
+
+def run_reddit_bot():
+    bot = PCPPHelperBot()
+    bot.monitor_subreddit('buildapc')
+
+
+if __name__ == '__main__':
+    pcpp = PCPartPickerList()
+    
+    page_html, url = run_parse_local()
     pcpp.parse_page(page_html)
     pcpp.print_parts()
     print(pcpp.total)
     
-    MC = MarkupCreator(test_url, pcpp.parts_list, pcpp.total)
+    MC = MarkupCreator(url, pcpp.parts_list, pcpp.total)
     MC.create_markup_table()
     print(MC.markup)
     
-    """
-    bot = PCPPHelperBot()
-    bot.monitor_subreddit('buildapc')
-    """
+    # run_reddit_bot()
