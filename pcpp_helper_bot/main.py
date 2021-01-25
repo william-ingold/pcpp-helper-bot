cbd7cce3ac715fd9977c9c769d5fae381637e2f2
William Ingold <github.ingold@gmail.com>
2021-01-25 11:13:29 -0500

    Setup to be live on development subreddit

--- pcpp_helper_bot/main.py
+++ pcpp_helper_bot/main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
     f_handler = logging.FileHandler(f'../logs/{date}.log', mode='w', encoding='utf-8')
     
-    bot = PCPPHelperBot(f_handler)
-    bot.monitor_subreddit('buildapc')
+    bot = PCPPHelperBot(f_handler, live=True)
+    bot.monitor_subreddit('buildapcdev')
     
     f_handler.close()
